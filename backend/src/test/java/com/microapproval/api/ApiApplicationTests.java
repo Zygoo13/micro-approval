3f9aef29821ce09515a9d3983f76327cb4172066
Hoang Quoc Anh <AnhHQ.B23CN022@stu.ptit.edu.vn>
2026-07-14 14:58:29 +0700

    Init: set up project monorepo structure

---
+++ backend/src/test/java/com/microapproval/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microapproval.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
